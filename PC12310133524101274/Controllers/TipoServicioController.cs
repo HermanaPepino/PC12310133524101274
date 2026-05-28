bcd43064808069b4657ba3dcb2adc63a996b2990
Julio <laboratorioesan@AO2300927>
2026-05-28 18:58:16 -0500

    Pregunta 4 Controlador TipoServicio

---
+++ PC12310133524101274/Controllers/TipoServicioController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+
+namespace PC12310133524101274.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoServicioController: ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok("GetAll");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok($"GetById: {id}");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return Ok("Create");
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id)
+        {
+            return Ok($"Update: {id}");
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok($"Delete: {id}");
+        }
+
+    }
+}
---
+++ TALLERMECÁNICO.CORE/Core/DTOs/TipoServicioDTO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace TALLERMECÁNICO.CORE.Core.DTOs
+{
+    public class TipoServicioDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+    }
+    public class TipoServicioListDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+    }
+    public class TipoServicioCreateDTO
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; } = 0;
+    }
+    public class TipoServicioUpdateDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; } = 0;
+    }
+    public class TipoServicioDeleteDTO
+    {
+        public int Id { get; set; }
+    }
+
+
+    }
