914a4002fa553144f1be12c7236408d901d0a61e
Juliet <23101335@ue.edu.pe>
2026-05-28 19:18:07 -0500

    cambios

---
+++ TALLERMECÁNICO.CORE/Infraestructure/Repositories/OrdenServicioRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TALLERMECÁNICO.CORE.Infraestructure.Repositories
+{
+    internal class OrdenServicioRepository
+    {
+    }
+}
